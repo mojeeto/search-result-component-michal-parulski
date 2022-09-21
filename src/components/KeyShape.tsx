dfed49f853446624b24af66649e8ec0f8709c18e
Mojeeto <mojtaba01nouri@gmail.com>
2022-09-21 21:33:43 +0430

    addEventListener for focus input

---
+++ src/components/KeyShape.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { KeyShapeProps } from "./interface";
+
+const KeyShape: React.FC<KeyShapeProps> = ({ character }) => {
+  return (
+    <div className="h-9 w-9 z-10 border-2 border-stone-300 border-b-stone-200 relative flex items-center justify-center text-lg rounded-xl">
+      {character.charAt(0)}
+      <div className="h-5 w-9 z-0 border-2 border-stone-300 border-t-0 rounded-b-xl absolute -bottom-1.5"></div>
+    </div>
+  );
+};
+
+export default KeyShape;
--- src/components/SearchModalComponent.tsx
+++ src/components/SearchModalComponent.tsx
@@ -1,35 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import KeyShape from "./KeyShape";
+import SearchIcon from "./SearchIcon";
 
 const SearchModalComponent: React.FC = () => {
-  // good initial height is 64px
-  const [searchComponentHeight, setSearchComponentHeight] =
-    useState<number>(64);
+
+  const [inputSearchValue, setInputSearchValue] = useState<string>("");
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const focusInput = () => {
+    const target = searchInputRef.current;
+    if (target) {
+      target.focus();
+      window.removeEventListener("keypress", eventListenerKeyPress);
+    }
+  }
+
+  const onChangeSearchInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const target = e.currentTarget;
+    if (target) {
+      const newValue = target.value;
+      setInputSearchValue(newValue);
+    }
+  }
+
+  const onBlurSearchInput: React.ChangeEventHandler<HTMLInputElement> = (_) => {
+    window.addEventListener("keypress", eventListenerKeyPress);
+  }
+
+  const eventListenerKeyPress = (e: KeyboardEvent) => {
+    const keyTarget = e.code;
+    if (keyTarget === "KeyS") {
+      focusInput();
+    }
+    console.log("live")
+  }
+
+  useEffect(() => {
+    if (searchInputRef) {
+      window.addEventListener('keypress', eventListenerKeyPress)
+    }
+  }, []);
+
   return (
-    <div
-      className="flex flex-col py-5 px-3 bg-white rounded-lg text-md transition-[height] duration-1000"
-      style={{ height: `${searchComponentHeight}px` }}
-    >
-      <div className="flex gap-2 items-center text-stone-400 w-[350px] md:w-[500px]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
+    <div className="flex flex-col w-[350px] md:w-[500px] py-5 px-3 bg-white rounded-xl text-[18px] md:text-xl transition-[height] duration-1000 shadow-stone-400 shadow-xl">
+      <div className="flex gap-2 items-center text-stone-400">
+        <SearchIcon />
         <input
           type="text"
           placeholder="Searching is easier"
-          className="border-none outline-none text-stone-800 placeholder:font-normal font-medium flex-1"
+          className="border-none outline-none text-stone-800 placeholder:font-normal placeholder:text-stone-400 font-medium flex-1"
+          ref={searchInputRef}
+          value={inputSearchValue}
+          onChange={onChangeSearchInput}
+          onBlur={onBlurSearchInput}
         />
-        <span className="hidden md:block">quick access</span>
+        <div className="hidden md:flex text-sm items-center gap-2">
+          <KeyShape character="s" />
+          <span>quick access</span>
+        </div>
       </div>
     </div>
   );
