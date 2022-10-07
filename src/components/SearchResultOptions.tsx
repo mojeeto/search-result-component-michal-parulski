695a4d6eec4ef1f8510f7711f22595976e2fc019
Mojeeto <mojtaba01nouri@gmail.com>
2022-10-07 18:38:38 +0330

    complete project

--- src/components/SearchResultOptions.tsx
+++ src/components/SearchResultOptions.tsx
@@ -1,13 +1,20 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import CustomText from "./utils/CustomText";
 import DynamicNumber from "./DynamicNumber";
 import { TypesCount } from "./SearchResultSection";
 import SettingOption from "./SettingOption";
-import { SearchModalContext } from "./SearchModalContext";
+import {
+  SearchModalContext,
+  SearchModalOptionType,
+} from "./SearchModalContext";
 
 interface SearchResultOptionsProps {
   typesCount: TypesCount;
 }
+
+type activeOptionType = {
+  option: SearchModalOptionType | null;
+};
 
 const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
   typesCount,
@@ -15,8 +22,14 @@ const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
   const underlineRef = useRef<HTMLDivElement>(null);
   const listRef = useRef<HTMLDivElement>(null);
   const { searchModalData } = useContext(SearchModalContext)!;
+  const [activeOption, setActiveOption] = useState<activeOptionType>({
+    option: null,
+  });
 
-  const handleUnderlineRef: React.MouseEventHandler<HTMLDivElement> = (e) => {
+  const handleUnderlineRef = (
+    e: React.MouseEvent<HTMLDivElement>,
+    option: SearchModalOptionType | null
+  ) => {
     const parent = listRef.current;
     const underline = underlineRef.current;
     if (!parent || !underline) {
@@ -26,22 +39,15 @@ const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
     const uWidth = target.offsetWidth + 5;
     const targetXPosition = target.getBoundingClientRect().x;
     const parentXPosition = parent.getBoundingClientRect().x;
-    const uXPosition = targetXPosition - parentXPosition + 28;
+    const uXPosition = targetXPosition - parentXPosition + 29;
     underline.style.width = `${uWidth}px`;
     underline.style.left = `${uXPosition}px`;
 
-    const parentChildrenCount = parent.children.length;
-    for (let i = 0; i < parentChildrenCount; ++i) {
-      const child = parent.children.item(i);
-      if (!child) {
-        break;
-      }
-      const activeClassName = "!text-gray-700";
-      if (child.classList.contains(activeClassName)) {
-        child.classList.remove(activeClassName);
-      }
-      target.classList.add(activeClassName);
-    }
+    setActiveOption({ option: option });
+  };
+
+  const onScroll = () => {
+    console.log("Scroll");
   };
 
   return (
@@ -53,10 +59,14 @@ const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
         <div
           className="flex items-center gap-5 pb-1 text-gray-400 scrollbar-hide overflow-x-scroll"
           ref={listRef}
+          onScroll={onScroll}
         >
           <CustomText
-            className="cursor-pointer !text-gray-700"
-            onClick={handleUnderlineRef}
+            className={`cursor-pointer ${!activeOption.option ? "!text-gray-700" : ""
+              }`}
+            onClick={(e) =>
+              handleUnderlineRef(e as React.MouseEvent<HTMLDivElement>, null)
+            }
             tagContent={<DynamicNumber end={typesCount.all} />}
           >
             <span>All</span>
@@ -67,8 +77,14 @@ const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
               option.title === "People" ? typesCount.users : typesCount.files;
             return (
               <CustomText
-                className="cursor-pointer"
-                onClick={handleUnderlineRef}
+                className={`cursor-pointer ${activeOption.option === option ? "!text-gray-700" : ""
+                  }`}
+                onClick={(e) =>
+                  handleUnderlineRef(
+                    e as React.MouseEvent<HTMLDivElement>,
+                    option
+                  )
+                }
                 tagContent={<DynamicNumber end={counter} />}
                 key={index}
               >
@@ -79,12 +95,15 @@ const SearchResultOptions: React.FC<SearchResultOptionsProps> = ({
           })}
         </div>
         <div
-          className="border-b-2 border-black w-[57px] bg-black absolute transition-all -bottom-0.5 left-7"
+          className="border-b-2 border-black w-[57px] bg-black absolute transition-all -bottom-0.5 left-[29px]"
           ref={underlineRef}
         ></div>
         <SettingOption />
       </div>
-      <div className="w-full h-0.5 bg-gray-200"></div>
+      <div
+        className="hidden lg:block w-full h-0.5 bg-gray-200 animate-fadeUp"
+        style={{ animationDelay: "0.5s" }}
+      ></div>
     </div>
   );
 };
